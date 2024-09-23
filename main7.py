5310b5d05a957f600ca2c8ecb256697bc14447f1
Jaithra18 <Jaithrathota@gmail.com>
2024-09-23 18:15:30 +0100

    Add files via upload

---
+++ main7.py
@@ -0,0 +1,23 @@
+a=10
+b=-10
+if a!=b:
+    print("both are different")
+else:
+    print("they are the same")
+
+
+x=5
+y=10
+if x<=5 and y==10:
+    print("both the conditons are met")
+else:
+
+    print("the conditions are not met")
+    
+    
+c=6
+d=12
+if c<=6 or d>12:
+    print("One condition is met")
+else:
+    print("None of the conditions are met")
